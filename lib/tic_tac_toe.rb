0320a0f43192a885978115822803a4b8b16ae6ab
Jae Yong Kim <kim900180@gmail.com>
2018-07-25 03:04:58 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -28,7 +28,7 @@ class TicTacToe
   end
 
   def position_taken?(index)
-    @board[index] == "X" || board[index] == "O"
+    @board[index] == "X" || @board[index] == "O"
   end
 
   def valid_move?(index)
