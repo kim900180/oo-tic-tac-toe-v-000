96a353f4b307eea5cd8e13e9740356d0d4af77db
Jae Yong Kim <kim900180@gmail.com>
2018-07-25 02:52:06 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -86,9 +86,9 @@ class TicTacToe
     while !over?(board)
       turn(board)
     end
-    if won?(board)
-      puts "Congratulations #{winner(board)}!"
-    elsif draw?(board)
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
       puts "Cat's Game!"
     end
   end
