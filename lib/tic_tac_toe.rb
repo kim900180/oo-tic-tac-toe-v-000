61fb8ca794c69b6181acd90976e585912f8b088b
Jae Yong Kim <kim900180@gmail.com>
2018-07-25 03:02:43 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,7 +3,7 @@ class TicTacToe
     @board = board || Array.new(9, " ")
   end
 
-  def WIN_COMBINATIONS
+  # def WIN_COMBINATIONS
     @WIN_COMBINATIONS = [
     [0,1,2],
     [3,4,5],
@@ -14,7 +14,7 @@ class TicTacToe
     [0,4,8],
     [6,4,2]
   ]
-  end
+  # end
 
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
